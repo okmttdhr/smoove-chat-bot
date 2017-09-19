14fc12559e313259d9959d29e6d62a0ce851864a
okmttdhr <10101114fin@gmail.com>
2017-09-19 20:03:51 +0900

    reply back

--- handler.js
+++ handler.js
@@ -1,16 +1,43 @@
 'use strict';
 
+var https = require('https');
+
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+  console.log('event', event);
+  console.log('event.body', event.body);
+  console.log('event.body.events', event.body.events);
+  var b = JSON.parse(event.body);
+  console.log('b', b);
+  var message = b.events[0].message.text;
+  var replyToken = b.events[0].replyToken;
+  var data = JSON.stringify({
+    replyToken: replyToken,
+    messages: [
+      {
+        type: 'text',
+        text: message,
+      },
+    ],
+  })
+  var opts = {
+    host: 'api.line.me',
+    path: '/v2/bot/message/reply',
+    headers: {
+      "Content-type": "application/json",
+      "Authorization": "Bearer"
+    },
+    method: 'POST',
   };
+  var req = https.request(opts, function(res){
+    res.on('data', function (chunk) {
+      console.log(res.statusCode + chunk.toString());
+    });
+    req.on('error', function(err) {
+      console.log('https.request error', err);
+      console.log('https.request error', err.message);
+    });
+  });
+  req.write(data);
+  req.end();
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
